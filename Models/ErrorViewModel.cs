4b9f53448deb4bbb1d5d0864af0e95e647991c1e
tealcoderotis <parallelcircuit17@gmail.com>
2025-02-22 11:26:04 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace ActivityTracker.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
